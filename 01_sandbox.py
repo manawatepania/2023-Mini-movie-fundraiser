f4b328940cc8373dc701ac72c130df874a199893
manawatepania <131836842+manawatepania@users.noreply.github.com>
2023-05-16 09:50:02 +1200

    Add files via upload

--- 01_sandbox.py
+++ 01_sandbox.py
@@ -1,1 +1,17 @@
-print("hello world")
+# functions go here
+
+
+# main routine goes here
+
+while True:
+    want_instructions = input("Do you want to read the "
+                          "instructions? ").lower()
+
+    if want_instructions == "yes" or want_instructions == "y":
+        print("instructions go here")
+    elif want_instructions == "no" or want_instructions == "n":
+       pass
+    else:
+        print("please answer yes / no")
+
+print("we are done")
